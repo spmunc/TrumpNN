48fa24088a11ef0ec5b34b1480bd6ff83302fcd2
Christina Rokholt <chr4@rice.edu>
2016-03-29 16:53:46 -0500

    change

--- trumpNN.R
+++ trumpNN.R
@@ -3,7 +3,7 @@
 ##  trumpNN.R: fits a neural network with one hidden layer to predict
 ##             the fraction of the votes in each county in NH, IA, SC, NV
 ##             that Donald Trump recieved based on demographics
-## Christina is the best
+## things
 ##
 ###############################################################################
 
